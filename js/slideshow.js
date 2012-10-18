1c14ae715edf20fe0c7e1a5309b6af5bf02fe1b4
unknown <ratheesh.naithalath@lntinfotech.com>
2012-10-18 11:47:10 +0530

    modified to carousel

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,45 +1,60 @@
-var slideshow= function(){
-	var itemHeight,
-		itemCount,
-		slideWidth,
-		containerWidth,
-		slideNumber= -1,
-		shiftWidth = 0,
-		shiftSide = 0,
-		shiftOffset = 0;
-	return {
-		init : function(){
-			itemHeight = $('.container .items .item').outerHeight();
-			itemCount = $('.container .items .item').length;
-			slideWidth = $('.container').outerWidth();
-			containerWidth = itemCount*slideWidth;
-			shiftOffset = parseInt($('.item:not(".first")').css('marginLeft'));
-			shiftWidth = parseInt($('.container').width());
-			shiftSide = shiftOffset+shiftWidth;
-			$('.container .items').width(containerWidth);
-			slideshow.next();
-		},
-		next : function() {
-			slideNumber = slideNumber+1 == itemCount ? 0 : slideNumber+1;
-			var moveLeft = slideNumber * shiftSide * -1;
-			$('.container .items').css('marginLeft',moveLeft);
-			$('.item').removeClass('current').eq(slideNumber+1).addClass('current');
-		},
-		previous : function() {
-			var moveRight = parseInt($('.container .items').css('marginLeft'));
-			moveRight = slideNumber == 0 ? (itemCount-1) * shiftSide * -1 : moveRight + shiftSide;
-			$('.container .items').css('marginLeft',moveRight);
-			slideNumber = slideNumber-1 < 0 ? itemCount -1 : slideNumber -1 ;
-			$('.item').removeClass('current').eq(slideNumber-1).addClass('current');
+(function($){
+	var initialize = function(container) {
+		var itemWidth = container.outerWidth();
+		$('.items',container).width(container.attr('itemCount') * itemWidth);
+		next(container);
+	}
+	var next = function(container) {
+		var slideNumber = parseInt(container.attr('slideNumber')) +1 == container.attr('itemCount') ? 0 : parseInt(container.attr('slideNumber'))+1;
+		container.attr('slideNumber',slideNumber);
+		var moveLeft = slideNumber * container.attr('shiftSide') * -1;
+		$('.items',container).css('marginLeft',moveLeft);
+		$('.items .item',container).removeClass('current').eq(slideNumber+1).addClass('current');
+	}
+	var previous = function(container) {
+		var moveRight = parseInt($('.items',container).css('marginLeft'));
+			moveRight = parseInt(container.attr('slideNumber')) == 0 ? (container.attr('itemCount')-1) * container.attr('shiftSide') * -1 : moveRight + parseInt(container.attr('shiftSide'));
+			$('.items',container).css('marginLeft',moveRight);
+			var slideNumber = parseInt(container.attr('slideNumber'))-1 < 0 ? container.attr('itemCount') -1 : parseInt(container.attr('slideNumber')) -1 ;
+			container.attr('slideNumber',slideNumber);
+			$('.items .item',container).removeClass('current').eq(slideNumber-1).addClass('current');
+	}
+	var attachControls = function(container) {
+		$('.next',container).on('click', {container:container},function(event){
+			next(event.data.container);
+		});
+		$('.previous',container).on('click',{container:container},function(event){
+			previous(event.data.container);
+		});
+	}
+	var methods = {
+		init: function() {
+			this.addClass('container');
+			$('.items .item',this).eq(0).addClass('first');
+			this.attr({'itemCount':$('.items .item',this).length,
+						'shiftSide':parseInt($('.item:not(".first")',this).css('marginLeft'))+parseInt(this.width()),
+						'slideNumber': -1
+					});
+			initialize(this);
+			attachControls(this);
+			return this;
 		}
 	}
-}();
-jQuery(function(){
-	slideshow.init();
-	jQuery('.next').on('click',function(){
-		slideshow.next();
-	});
-	jQuery('.previous').on('click',function(){
-		slideshow.previous();
-	});
+	
+	$.fn.uCarousel = function( method ) {
+    // Method calling logic
+    if ( methods[method] ) {
+      return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
+    } else if ( typeof method === 'object' || ! method ) {
+      return methods.init.apply( this, arguments );
+    } else {
+      $.error( 'Method ' +  method + ' does not exist on jQuery.uCarousel' );
+    }    
+  
+  };
+})(jQuery);
+
+jQuery(function() {
+	$('.container').uCarousel();
+	$('.secondCarousel').uCarousel();
 });
